4502b5db6d7aa7590d0729694f1454079933b57b
durantecode <durantecode@gmail.com>
2022-05-19 16:40:06 +0200

    cpp02 passed

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 20:35:56 by ldurante          #+#    #+#             */
-/*   Updated: 2022/05/18 21:59:17 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/05/19 14:28:19 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int main(void)
 	Fixed c(10);
 	Fixed d(5);
 
-	if (c > d)
-		std::cout << "TRUE" << std::endl;
+	std::cout << "----------- BASIC OPERATIONS ---------" << std::endl;
+	std::cout << "TRUE: " << (c > d) << std::endl;
+	std::cout << "FALSE: " << (c < d) << std::endl;
 
 	std::cout << c + d << std::endl;
 	std::cout << c - d << std::endl;
@@ -28,17 +29,19 @@ int main(void)
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+	std::cout << "----------- OPERATOR OVERLOAD ---------" << std::endl;
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << "MAX: " << Fixed::max( a, b ) << std::endl;
+	std::cout << "MIN: " << Fixed::min( a, b ) << std::endl;
 	// std::cout << --a << std::endl;
 	// std::cout << a-- << std::endl;
 	// std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
 
 	return (0);
 }
